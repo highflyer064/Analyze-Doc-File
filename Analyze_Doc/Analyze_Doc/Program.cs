f7c3f1efae463b7014f62f1b164e75a1c0b836ba
High Flyer <highflyer064@gmail.com>
2025-06-24 05:59:30 -0700

    create project

---
+++ Analyze_Doc/Analyze_Doc/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Reflection.Metadata;
+using static System.Net.Mime.MediaTypeNames;
+using Word = Microsoft.Office.Interop.Word;
+
+class Program
+{
+    static void Main()
+    {
+        Word.Application word = new Word.Application();
+        Word.Document doc = null;
+
+        try
+        {
+            object filePath = @"E:\11.doc";
+            object readOnly = true;
+            object missing = System.Reflection.Missing.Value;
+
+            doc = word.Documents.Open(ref filePath, ReadOnly: ref readOnly, Visible: false);
+            string text = doc.Content.Text;
+            Console.WriteLine(text);
+        }
+        finally
+        {
+            if (doc != null) doc.Close();
+            word.Quit();
+        }
+    }
+}
